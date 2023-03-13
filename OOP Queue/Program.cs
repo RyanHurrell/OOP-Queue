4c32a46b1dccff4ebb5ad79035a720f0a23966a3
Ryan Hurrell <17HurrR1@student.allhallows.net>
2023-03-13 11:24:36 +0000

    Add project files.

---
+++ OOP Queue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = new List<string>();
+            name.Add("Chris");
+            name.Add("Kevin");
+            name.Add("Alan");
+            name.Add("Bringo");
+            name.Add("Chiney");
+            string action = "";
+
+
+            Queue1 Queue = new Queue1(0, 4, 0, 1, name, 0);
+
+            while (action != "end")
+            {
+                Console.WriteLine("what would you like to do");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "Add":
+                        Queue.Add();
+                        break;
+
+                    case "Remove":
+                        Queue.Remove();
+                        break;
+
+                    case "Read":
+                        Queue.Read();
+                        break;
+
+                    case "isFull":
+                        Queue.is_Full();
+                        break;
+                }
+            }
+        
+
+        }
+    }
+}
---
+++ OOP Queue/Queue1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Queue
+{
+    class Queue1
+    {
+
+        public int start = 0;
+        public int end = 4;
+        public int isFull = 0;
+        public int space = 1;
+        public string action = "";
+        public int full;
+        List<string> name = new List<string>();
+
+
+        public Queue1(int start, int end, int isFull, int space, List<string> name, int full)
+        {
+            this.start = start;
+            this.end = end;
+            this.name = name;
+            this.full = full;
+            this.space = space;
+            this.isFull = isFull;
+
+        }
+        public void Add()
+        {
+            full = 0;
+            Console.WriteLine("What would you like to add");
+            string names = Console.ReadLine();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                if (name[i] == "")
+                {
+                    name[i] = names;
+                    end = i;
+                    i = 5;
+                }
+                else
+                {
+                    full++;
+                }
+
+                if(full == 5)
+                {
+                    Console.WriteLine("Queue is full!");
+                }
+            }
+        }
+
+        public void Remove()
+        {
+            name[start] = "";
+            if (start != 4)
+            {
+                start = start + 1;
+            }
+            else
+            {
+                start = 0;
+            }
+        }
+        public void is_Full()
+        {
+             for(int i = 0; i <= 4; i++)
+      {
+        if(name[i] != "")
+        {
+          isFull++;
+        }
+        else
+        {
+          Console.WriteLine("the queue has " + space + " spaces");
+          space++;
+        }
+      }
+      if(isFull == 5)
+      {
+        Console.WriteLine("the queue is full");
+      }
+        }
+        public void Read()
+        {
+            Console.WriteLine("Your current queue is: ");
+            foreach (string i in name)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
